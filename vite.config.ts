3141fb8eedef7e808c5e80d5b6dbe2b8b242b67d
Ho3einWave <ho3ein8280@gmail.com>
2024-10-07 10:13:15 +0330

    fix: inject css

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        cssInjectedByJsPlugin(),
         tsconfigPaths(),
         react(),
         dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
